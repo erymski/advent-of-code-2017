eaba0117bd69a47cb22fd8370d1dfa68a64e63fe
Eugene Rymski <erymski@gmail.com>
2017-12-23 01:57:22 +0300

    Day 15

---
+++ day15/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong genA = 116; //65;
+            ulong genB = 299; //8921;
+
+            var res1 = Task1(genA, genB);
+            Console.WriteLine(res1);
+
+            var res2 = Task2(genA, genB);
+            Console.WriteLine(res2);
+
+            Console.ReadLine();
+        }
+
+        private static int Task1(ulong genA, ulong genB)
+        {
+            int judge = 0;
+
+            for (int i = 0; i < 40000000; i++)
+            {
+                genA = (genA * 16807) % 2147483647;
+                genB = (genB * 48271) % 2147483647;
+
+                if ((genA & 0xffff) == (genB & 0xffff))
+                {
+                    judge++;
+                }
+            }
+
+            return judge;
+        }
+
+        private static int Task2(ulong genA, ulong genB)
+        {
+            int judge = 0;
+
+            for (int i = 0; i < 5000000; i++)
+            {
+                do
+                {
+                    genA = (genA * 16807) % 2147483647;
+                } while (genA % 4 != 0);
+
+                do
+                {
+                    genB = (genB * 48271) % 2147483647;
+                } while (genB % 8 != 0);
+
+                if ((genA & 0xffff) == (genB & 0xffff))
+                {
+                    judge++;
+                }
+            }
+
+            return judge;
+        }
+    }
+}
