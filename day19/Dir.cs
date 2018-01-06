786ecfc9eda4789096fa6081c5d4344bb036831e
Eugene Rymski <erymski@gmail.com>
2018-01-06 17:22:13 +0300

    Day 19

---
+++ day19/Dir.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace day19
+{
+    /// <summary>
+    /// Represents both position and direction.
+    /// Direction is unit vector.
+    /// </summary>
+    struct Dir
+    {
+        public int x;
+        public int y;
+
+        /// <summary>
+        /// Add vectors.
+        /// </summary>
+        public Dir Add(Dir dir)
+        {
+            return new Dir { x = x + dir.x, y = y + dir.y };
+        }
+
+        /// <summary>
+        /// Inverse direction.
+        /// </summary>
+        private Dir Negate()
+        {
+            return new Dir { x = x * -1, y = y * -1 };
+        }
+
+        /// <summary>
+        /// Rotate 90 degrees.
+        /// </summary>
+        private Dir Rotate()
+        {
+            var newX = y; // dx * 0 + dy * 1;
+            var newY = x; // dx * 1 + dy * 0;
+            return new Dir { x = newX, y = newY };
+        }
+
+        /// <summary>
+        /// Get cells on left and right sides.
+        /// </summary>
+        public IEnumerable<Dir> Sides()
+        {
+            // take current direction and rotate 90 degrees
+            Dir rotated = Rotate();
+            yield return rotated;
+
+            // and then rotate result 180 degrees (270 degrees of original direction)
+            yield return rotated.Negate();
+        }
+    }
+}
---
+++ day19/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+
+namespace day19
+{
+    class Program
+    {
+        //private static readonly string[] rows = {
+        //                                        "     |         ",
+        //                                        "     |  +--+   ",
+        //                                        "     A  |  C   ",
+        //                                        " F---|----E|--+",
+        //                                        "     |  |  |  D",
+        //                                        "     +B-+  +--+"
+        //                                    };
+
+        private const char EmptySpace = ' ';
+        private const char HorizontalStep = '-';
+        private const char VerticalStep = '|';
+        private const char Turn = '+';
+
+        static void Main()
+        {
+            var dataPath = Path.Combine(Environment.CurrentDirectory, @"..\..", "input.txt");
+            var rows = File.ReadAllLines(dataPath);
+
+            var rowCount = rows.Length;
+            var columnCount = rows[0].Length;
+
+            // utility func to check if position is within input
+            Func<Dir, bool> isValid = d => d.y >= 0 &&
+                                              d.y < rowCount &&
+                                              d.x >= 0 &&
+                                              d.x < columnCount;
+
+            // utility func to get char under position
+            Func<Dir, char> getChar = d => rows[d.y][d.x];
+
+
+            // start position is first row (from top) and the only cell, with '|' in it.
+            var pos = new Dir { x = rows[0].IndexOf(VerticalStep), y = 0 };
+
+            // Vector with direction. Initial direction is go down
+            var dir = new Dir { x = 0, y = 1 };
+
+
+            int steps = 0;
+            while (true)
+            {
+                steps++;
+
+                pos = pos.Add(dir);
+                char currChar = getChar(pos);
+
+                // check if it's end of the path
+                if (currChar == EmptySpace) break;
+
+                // just follow path
+                if (currChar == HorizontalStep || currChar == VerticalStep) continue;
+
+                // time to change dir
+                if (currChar == Turn)
+                {
+                    // find turn direction
+                    foreach (var sideDir in dir.Sides())
+                    {
+                        Dir candidatePos = pos.Add(sideDir);
+                        if (! isValid(candidatePos)) continue;
+
+                        char candidateChar = getChar(candidatePos);
+                        if (candidateChar == EmptySpace) continue;
+
+                        // candidate cell contains the further trail, so change direction
+                        dir = sideDir;
+                        break;
+                    }
+                    continue;
+                }
+
+                // unchecked chars are letters. Dump it
+                Console.Write(currChar);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(steps);
+
+            Console.ReadLine();
+        }
+    }
+}
